98589bd2c25c2b7224bfd87cbfe5a5edc15fb6a0
Assaduzzaman Assad <assaduzzaman.assad@yrbrands.com>
2017-12-16 21:20:22 +0600

    spring datd jpa

--- src/main/java/com/dev/repository/GoalRepository.java
+++ src/main/java/com/dev/repository/GoalRepository.java
@@ -2,14 +2,18 @@ package com.dev.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import com.dev.model.Goal;
 import com.dev.model.GoalReport;
 
-public interface GoalRepository {
-	Goal save(Goal goal);
-
-	List<Goal> loadAll();
-
+@Repository("goalRepository")
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+	
+	@Query("Select new com.dev.model.GoalReport(g.minutes, e.minutes, e.activity) " +
+			"from Goal g, Exercise e where g.id = e.goal.id")
 	List<GoalReport> findAllGoalReports();
-
+	
 }
--- src/main/java/com/dev/service/GoalServiceImpl.java
+++ src/main/java/com/dev/service/GoalServiceImpl.java
@@ -20,7 +20,7 @@ public class GoalServiceImpl implements GoalService {
 	}
 
 	public List<Goal> findAllGoals() {
-		return goalRepository.loadAll();
+		return goalRepository.findAll();
 	}
 
 	public List<GoalReport> findAllGoalReports() {
