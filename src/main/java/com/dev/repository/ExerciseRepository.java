46fe71431df632ced7afe55c11f08da83fe487dd
Assaduzzaman Assad <assaduzzaman.assad@yrbrands.com>
2017-12-16 21:10:09 +0600

    spring jpa data example

--- src/main/java/com/dev/repository/ExerciseRepository.java
+++ src/main/java/com/dev/repository/ExerciseRepository.java
@@ -1,7 +1,9 @@
 package com.dev.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.dev.model.Exercise;
 
-public interface ExerciseRepository {
-	Exercise save (Exercise exercise);
+public interface ExerciseRepository extends JpaRepository<Exercise, Long>{
+	
 }
